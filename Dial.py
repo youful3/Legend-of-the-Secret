ca2cf012463e732d22084027d7df931bf3a90d77
youful3 <adeel29we@outlook.com>
2022-12-20 15:23:41 +0500

    Added a title :D

--- Dial.py
+++ Dial.py
@@ -1,3 +1,5 @@
+import os
+
 def v1(strs):
     input(f"Voice 1: {strs}")
     print("")
@@ -31,3 +33,24 @@ v2("Look at me, and look at you, what am I and what are you?")
 v1("I'm a faulty machine, who can't even do what it was designed to: kill.")
 v2("And me?")
 v1("You're sick.")
+v2("I expected nothing more from someone like you.")
+v2("You try too hard, you try too hard to be something you were never supposed to be.")
+v1("You were never like this, never...")
+v2("People change, you've become more soft.")
+v1("What we did was wrong...")
+v2("The world isn't black and white.")
+v1("Look who's talking!")
+v2("Enough!")
+v1("I will make you pay.")
+
+c("Silence...")
+c("The muffled sounds of clashing are heard...")
+c("It's time to wake up...")
+c("The world isn't black and white...")
+c("You weren't supposed to see this...")
+c("Wake up...")
+c("Wake up... ")
+
+os.startfile("Title.py")
+
+exit()
---
+++ Title.py
@@ -0,0 +1,16 @@
+def g():
+    print("")
+
+import time
+time.sleep(3)
+g()
+print("A Youful3MakesBadGames (Y3MBG) Production")
+time.sleep(5)
+g()
+print("Based on the level in 'There Is No Game' by 'Draw Me A Pixel'")
+g()
+time.sleep(6)
+print("Legend of the Secret")
+g()
+time.sleep(5)
+input("Press [Enter] to continue: ")
